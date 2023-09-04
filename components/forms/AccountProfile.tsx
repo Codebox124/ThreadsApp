72317975615f4f7ef75a584a3a5db445a28bce38
codebox124 <rajcodes733@gmail.com>
2023-09-04 11:24:13 +0100

    bugs

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,10 +19,10 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 
-// import { useUploadThing } from "@/lib/uploadthing";
-// import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from "@/lib/uploadthing";
+import { isBase64Image } from "@/lib/utils";
 
-// import { UserValidation } from "@/lib/validations/user";
+import { UserValidation } from "@/lib/validations/user";
 // import { updateUser } from "@/lib/actions/user.actions";
 
 interface Props {
@@ -66,14 +66,14 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
       }
     }
 
-    await updateUser({
-      name: values.name,
-      path: pathname,
-      username: values.username,
-      userId: user.id,
-      bio: values.bio,
-      image: values.profile_photo,
-    });
+    // await updateUser({
+    //   name: values.name,
+    //   path: pathname,
+    //   username: values.username,
+    //   userId: user.id,
+    //   bio: values.bio,
+    //   image: values.profile_photo,
+    // });
 
     if (pathname === "/profile/edit") {
       router.back();
